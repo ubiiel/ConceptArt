1383dfe0e1e339a8280f3484f4d7c1c8134947a5
Gabriel Matos <81238494+ubiiel@users.noreply.github.com>
2025-01-15 00:14:49 -0300

    Add files via upload

---
+++ site-CA/script.js
@@ -0,0 +1,159 @@
+
+document.getElementById('cora-vazio1').addEventListener('click', function() {
+ const icon = this;
+
+ icon.classList.add('animate');
+
+ if (icon.classList.contains('fa-regular')) {
+      icon.classList.remove('fa-regular')
+      icon.classList.add('fa-solid')
+ }
+ else {
+      icon.classList.remove('fa-solid')
+      icon.classList.add('fa-regular')
+ }
+ });
+
+
+document.getElementById('cora-vazio2').addEventListener('click', function() {
+     const icon = this;
+    
+     icon.classList.add('animate');
+    
+     if (icon.classList.contains('fa-regular')) {
+          icon.classList.remove('fa-regular')
+          icon.classList.add('fa-solid')
+     }
+     else {
+          icon.classList.remove('fa-solid')
+          icon.classList.add('fa-regular')
+     }
+     });
+
+
+document.getElementById('cora-vazio3').addEventListener('click', function() {
+     const icon = this;
+    
+     icon.classList.add('animate');
+    
+     if (icon.classList.contains('fa-regular')) {
+          icon.classList.remove('fa-regular')
+          icon.classList.add('fa-solid')
+     }
+     else {
+          icon.classList.remove('fa-solid')
+          icon.classList.add('fa-regular')
+     }
+     });
+     
+     
+document.getElementById('cora-vazio4').addEventListener('click', function() {
+ const icon = this;
+
+ icon.classList.add('animate');
+
+ if (icon.classList.contains('fa-regular')) {
+      icon.classList.remove('fa-regular')
+      icon.classList.add('fa-solid')
+ }
+ else {
+      icon.classList.remove('fa-solid')
+      icon.classList.add('fa-regular')
+ }
+ });
+
+
+document.getElementById('cora-vazio5').addEventListener('click', function() {
+     const icon = this;
+    
+     icon.classList.add('animate');
+    
+     if (icon.classList.contains('fa-regular')) {
+          icon.classList.remove('fa-regular')
+          icon.classList.add('fa-solid')
+     }
+     else {
+          icon.classList.remove('fa-solid')
+          icon.classList.add('fa-regular')
+     }
+     });
+
+
+document.getElementById('cora-vazio6').addEventListener('click', function() {
+     const icon = this;
+    
+     icon.classList.add('animate');
+    
+     if (icon.classList.contains('fa-regular')) {
+          icon.classList.remove('fa-regular')
+          icon.classList.add('fa-solid')
+     }
+     else {
+          icon.classList.remove('fa-solid')
+          icon.classList.add('fa-regular')
+     }
+     });
+
+
+document.getElementById('cora-vazio7').addEventListener('click', function() {
+ const icon = this;
+
+ icon.classList.add('animate');
+
+ if (icon.classList.contains('fa-regular')) {
+      icon.classList.remove('fa-regular')
+      icon.classList.add('fa-solid')
+ }
+ else {
+      icon.classList.remove('fa-solid')
+      icon.classList.add('fa-regular')
+ }
+ });
+
+
+document.getElementById('cora-vazio8').addEventListener('click', function() {
+     const icon = this;
+    
+     icon.classList.add('animate');
+    
+     if (icon.classList.contains('fa-regular')) {
+          icon.classList.remove('fa-regular')
+          icon.classList.add('fa-solid')
+     }
+     else {
+          icon.classList.remove('fa-solid')
+          icon.classList.add('fa-regular')
+     }
+     });
+
+
+document.getElementById('cora-vazio9').addEventListener('click', function() {
+ const icon = this;
+
+ icon.classList.add('animate');
+
+ if (icon.classList.contains('fa-regular')) {
+      icon.classList.remove('fa-regular')
+      icon.classList.add('fa-solid')
+ }
+ else {
+      icon.classList.remove('fa-solid')
+      icon.classList.add('fa-regular')
+ }
+ });
+
+
+document.getElementById('cora-vazio10').addEventListener('click', function() {
+ const icon = this;
+
+ icon.classList.add('animate');
+
+ if (icon.classList.contains('fa-regular')) {
+      icon.classList.remove('fa-regular')
+      icon.classList.add('fa-solid')
+ }
+ else {
+      icon.classList.remove('fa-solid')
+      icon.classList.add('fa-regular')
+ }
+ });
